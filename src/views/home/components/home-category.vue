c943eedb0c753ae86d4c2a5c319275452d52ad25
JunXi Li <1664478831@qq.com>
2022-02-13 23:36:57 +0800

    封装骨架屏组件

--- src/views/home/components/home-category.vue
+++ src/views/home/components/home-category.vue
@@ -17,6 +17,15 @@
             {{ sub.name }}
           </RouterLink>
         </template>
+        <span v-else>
+          <XtxSkeleton
+            width="60px"
+            height="18px"
+            style="margin-right: 5px"
+            bg="rgba(255,255,255,0.2)"
+          />
+          <XtxSkeleton width="50px" height="18px" bg="rgba(255,255,255,0.2)" />
+        </span>
       </li>
     </ul>
     <!-- 弹层 -->
